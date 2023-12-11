7d61fbab656da0faa54ded18d1a668ed06bfae6b
Yuzyzy88 <ayu.sulistiowati@protonmail.com>
2023-12-11 10:42:32 +0700

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,6 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'sulistiowatiayu@gmail.com'
-EMAIL_HOST_PASSWORD = 'eajw ytvh ymmp ifvi'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
